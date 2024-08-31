6d6a125fa5a46d01e2f9be907e8d65f4f8ffbb6f
Valeriy Chernobrovyi <valerasergeevich@gmail.com>
2024-08-31 22:27:53 +0300

    Add functions: Servo driver and Sonar

---
+++ include/Sonar.h
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+
+// number of sonar pins
+int Trig;
+int Echo;
+
+int Sonar_init(int Tr, int Ec)
+{
+    Trig = Tr;
+    Echo = Ec;
+
+    pinMode(Trig, OUTPUT);
+    pinMode(Echo, INPUT);
+}
+
+int Sonar(unsigned long Limit)
+{
+    int Long_cm;
+
+    unsigned long Lim = Limit * 58;
+
+    digitalWrite(Trig, LOW);
+    delayMicroseconds(2);
+    digitalWrite(Trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(Trig, LOW);
+
+    Long_cm = int(pulseIn(Echo, HIGH, Lim)/58);
+
+    if(Long_cm == 0)
+        return int(Limit);
+    return Long_cm;
+}
---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+// Подключаем библиотеку управления сервомоторами
+#include <Servo.h>
+#include <Sonar.h>
+
+Servo neck;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hello, I'm robot named Yuppie ;)");
+  neck.attach(A5);
+
+  // Инициализируем дальномер Trig = A0, Echo = A1
+  Sonar_init(A0, A1);
+}
+
+int sonar_func() {
+  int prepyatstvie = Sonar(500);
+  Serial.print("Distance = ");
+  Serial.print(prepyatstvie);
+  Serial.println(" cm.");
+
+  return prepyatstvie;
+}
+
+void loop() {
+  int i;
+
+  for(i=10; i<=180; i=i+10)
+  {
+    neck.write(i);
+    sonar_func();
+    delay(1000);
+  }
+
+  for(i=180; i>=0; i=i-10)
+  {
+    neck.write(i);
+    sonar_func();
+    delay(1000);
+  }
+}
