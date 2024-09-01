d319f62854133f3cb9d6de95166bda5c620797f5
Valeriy Chernobrovyi <valerasergeevich@gmail.com>
2024-09-01 13:02:43 +0300

    Adding implementation of robot movement functions

--- src/main.cpp
+++ src/main.cpp
@@ -8,33 +8,9 @@
 // Подключаем библиотеку управления сервомоторами
 #include <Servo.h>
 #include <Sonar.h>
+#include <Motor.h>
 
 Servo neck;
-
-int motor_L1, motor_L2;
-int motor_R1, motor_R2;
-
-void setup_motor_system(int L1, int L2, int R1, int R2) {
-  motor_L1 = L1;
-  motor_L2 = L2;
-
-  motor_R1 = R1;
-  motor_R2 = R2;
-
-  pinMode(motor_L1, OUTPUT);
-  pinMode(motor_L2, OUTPUT);
-
-  pinMode(motor_R1, OUTPUT);
-  pinMode(motor_R2, OUTPUT);
-}
-
-void forward() {
-
-    digitalWrite(motor_L1, HIGH);
-    digitalWrite(motor_L2, LOW);
-    digitalWrite(motor_R1, HIGH);
-    digitalWrite(motor_R2, LOW);
-}
 
 void setup() {
   // put your setup code here, to run once:
@@ -45,10 +21,12 @@ void setup() {
   // Инициализируем дальномер Trig = A0, Echo = A1
   Sonar_init(A0, A1);
   setup_motor_system(0, 1, 2, 3);
+
+  _stop();
 }
 
 int sonar_func() {
-  int prepyatstvie = Sonar(500);
+  int prepyatstvie = Sonar(1000);
   Serial.print("Distance = ");
   Serial.print(prepyatstvie);
   Serial.println(" cm.");
@@ -58,19 +36,20 @@ int sonar_func() {
 
 void loop() {
   int i;
-
+/*
   for(i=10; i<=180; i=i+10)
   {
     neck.write(i);
     sonar_func();
-    forward();
-    delay(1000);
+    delay(2000);
   }
 
   for(i=180; i>=0; i=i-10)
   {
     neck.write(i);
     sonar_func();
-    delay(1000);
+    delay(2000);
   }
+*/
+  forward();
 }
