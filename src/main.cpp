6763adeb5ab0dc3d5e77c5acba61557172dac404
Valeriy Chernobrovyi <valerasergeevich@gmail.com>
2024-09-02 23:08:58 +0300

    Edit control motor's pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 /*
   Project to build a robot named Yuppie on base Arduino UNO R3.
-  version: 0.0.3
+  version: 0.0.4
   date: 28.08.2024
-  modified 31.08.2024
+  modified 01.09.2024
   Developer: Valeriy Chernobrovyi (chernobrovyivs)
 */
+
 // Подключаем библиотеку управления сервомоторами
 #include <Servo.h>
+// Подключаем библиотеку управления ультразвуковым датчиком
 #include <Sonar.h>
+// Подключаем библиотеку управления двигателями (4 двигателя)
 #include <Motor.h>
 
 Servo neck;
@@ -20,7 +23,7 @@ void setup() {
 
   // Инициализируем дальномер Trig = A0, Echo = A1
   Sonar_init(A0, A1);
-  setup_motor_system(0, 1, 2, 3);
+  setup_motor_system(4, 5, 6, 7);
 
   _stop();
 }
@@ -51,16 +54,17 @@ void loop() {
     delay(2000);
   }
 */
+
+  forward();
+  delay(5000);
+  forward_left();
+  delay(1000);
   forward();
   delay(1000);
-  forward_left();
-  delay(500);
-  forward();
-  delay(500);
   forward_right();
-  delay(500);
+  delay(1000);
   _stop();
-  delay(500);
+  delay(1000);
   backward();
   delay(800);
 }
